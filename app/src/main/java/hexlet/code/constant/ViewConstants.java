34c4c2cb1900e908b8abf479627ba3985849b64d
opifex <opifexM@outlook.com>
2023-04-30 21:55:25 -0400

    heroku added

---
+++ app/src/main/java/hexlet/code/constant/ViewConstants.java
@@ -0,0 +1,8 @@
+package hexlet.code.constant;
+
+public final class ViewConstants {
+    public static final String FORWARD_WELCOME = "forward:/welcome.html";
+
+    private ViewConstants() {
+    }
+}
