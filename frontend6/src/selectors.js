d5f127a6a3b05b1800921488d2b16e05245782e6
opifexM <94506319+opifexM@users.noreply.github.com>
2023-05-19 16:06:44 -0400

    Delete frontend6 directory

--- frontend6/src/selectors.js
+++
@@ -1,4 +0,0 @@
-// @ts-check
-export const getLabels = ({ labelsSlice }) => labelsSlice.labels;
-export const getStatuses = ({ statusesSlice }) => statusesSlice.statuses;
-export const getExecutors = ({ executorsSlice }) => executorsSlice.executors;
