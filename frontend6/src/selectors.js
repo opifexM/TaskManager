f9915dae80323d8d8aea48aaf8c25908a5c08355
opifex <opifexM@outlook.com>
2023-05-19 15:57:37 -0400

    frontend added

---
+++ frontend6/src/selectors.js
@@ -0,0 +1,4 @@
+// @ts-check
+export const getLabels = ({ labelsSlice }) => labelsSlice.labels;
+export const getStatuses = ({ statusesSlice }) => statusesSlice.statuses;
+export const getExecutors = ({ executorsSlice }) => executorsSlice.executors;
