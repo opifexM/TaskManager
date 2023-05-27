e45cf65b043156518169db3d93d56f8a3c170dd2
opifex <opifexM@outlook.com>
2023-05-27 14:49:23 -0400

    refactoring

--- src/main/java/hexlet/code/domain/task/TaskRepository.java
+++ src/main/java/hexlet/code/domain/task/TaskRepository.java
@@ -2,7 +2,6 @@ package hexlet.code.domain.task;
 
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.JpaSpecificationExecutor;
-import org.springframework.data.jpa.repository.Query;
 import org.springframework.stereotype.Repository;
 
 import java.util.Optional;
@@ -14,8 +13,6 @@ public interface TaskRepository extends JpaRepository<Task, Long>, JpaSpecificat
     Optional<Task> findByName(String name);
 
     boolean existsByLabels_Id(Long id);
-    @Query("select (count(t) > 0) from Task t inner join t.labels labels where labels.id = ?1")
-    boolean existsByLabelsId(Long id);
 
     boolean existsByTaskStatus_Id(Long id);
 
